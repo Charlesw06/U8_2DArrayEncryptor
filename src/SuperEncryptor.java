3f7629f91f9dc4c8b95de8449e52f47bca46a423
unknown <BT_4N2_24@BKTECH.LOCAL>
2023-03-09 12:33:15 -0500

    Initial commit

---
+++ src/SuperEncryptor.java
@@ -0,0 +1,13 @@
+public class SuperEncryptor {
+    public SuperEncryptor() {
+
+    }
+
+    public String superEncryptMessage(String message) {
+
+    }
+
+    public String superDecrpytMessage(String message) {
+
+    }
+}
